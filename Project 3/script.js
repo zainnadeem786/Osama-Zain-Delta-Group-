3c11b19c11724f8b0453d1a83442a5f8eb00a89b
Prousama <167557160+Prousama@users.noreply.github.com>
2024-05-13 21:13:40 +0500

    Delete Project 3/script.js

--- Project 3/script.js
+++
@@ -1,7 +0,0 @@
-const netflix_open_btn = document.querySelector('.cn-open-btn');
-const netflix_close_btn = document.querySelector('.cn-close-btn');
-const netflix_nav = document.querySelector('.cn-nav');
-
-netflix_open_btn.addEventListener('click', ()=> {
-    netflix_nav.forEach (nav_el=> {nav_el.classList.add('visible')});
-});
